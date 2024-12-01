02c0fec9428d3e37acb555144f06880f2aef8ce4
97Maykel97 <shalumov97@gmail.com>
2024-12-01 21:51:53 +0200

    added scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,34 @@ const shadowHeader = () => {
 window.addEventListener('scroll', shadowHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+	const scrollUp = document.getElementById('scroll-up');
+	this.scrollY >= 350
+		? scrollUp.classList.add('show-scroll')
+		: scrollUp.classList.remove('show-scroll');
+};
+window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
 
+const scrollActive = () => {
+	const scrollDown = window.scrollY;
+
+	sections.forEach(current => {
+		const sectionHeight = current.offsetHeight;
+		const sectionTop = current.offsetTop - 58;
+		const sectionId = current.getAttribute('id');
+		const sectionsClass = document.querySelector(
+			'.nav__menu a[href*=' + sectionId + ']'
+		);
+
+		if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+			sectionsClass.classList.add('active-link');
+		} else {
+			sectionsClass.classList.remove('active-link');
+		}
+	});
+};
+window.addEventListener('scroll', scrollActive);
 /*=============== SCROLL REVEAL ANIMATION ===============*/
